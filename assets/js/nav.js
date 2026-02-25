4a729fa5d5ddff7a41a780bff784ba45a9d4b12e
allure711 <zamente@gmail.com>
2026-02-25 15:44:09 -0500

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,1 +1,9 @@
+(() => {
+  const btn = document.querySelector(".nav__toggle");
+  const nav = document.querySelector(".nav__list");
+  if (!btn || !nav) return;
 
+  btn.addEventListener("click", () => {
+    nav.classList.toggle("is-open");
+  });
+})();
