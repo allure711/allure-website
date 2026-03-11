b0917ea26df8ae533f988c77229c22ce1bf677b6
allure711 <zamente@gmail.com>
2026-03-11 18:01:18 -0400

    Update menu-data.js

--- assets/js/menu-data.js
+++ assets/js/menu-data.js
@@ -94,7 +94,35 @@ items: [
 },
 
 {
-z
+{
+title: "Wing Flavors",
+layout: "wingsGrouped",
+groups: [
+
+{
+title: "Dry",
+items: [
+{ name: "Lemon Pepper" },
+{ name: "Jerk Rub" },
+{ name: "Old Bay" }
+]
+},
+
+{
+title: "Wet",
+items: [
+{ name: "Honey Lemon Pepper" },
+{ name: "Honey Old Bay" },
+{ name: "Buffalo BBQ" },
+{ name: "Honey Sazon" },
+{ name: "Sweet Chili" },
+{ name: "Teriyaki" },
+{ name: "Mumbo" }
+]
+}
+
+]
+}
 
 shots5: {
 sections: [
